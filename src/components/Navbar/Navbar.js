7511d1ecb914d04023daaa3ada8c5dbee065f9d8
mahfuzshamim7426 <mahfuzshamim7426@gmail.com>
2022-10-12 02:16:01 +0600

    navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     return (
         <div className='flex justify-between'>
             <div>
-                <h2 className='text-3xl text-orange-600 font-bold'>MS TECH QUIZ</h2>
+                <div>
+                    <h2 className='text-3xl text-orange-600 font-bold'>MS TECH QUIZ</h2>
+
+                </div>
             </div>
-            <nav className='text-xl flex flex-col md:flex-row'>
-                <Link className='ml-12 hover:text-orange-600' to='/'>Home</Link>
-                <Link className='ml-12 hover:text-orange-600' to='/statistic'>Statistic</Link>
-                <Link className='ml-12 hover:text-orange-600' to='/topics'>Topics</Link>
-                <Link className='ml-12 hover:text-orange-600' to='/blog'>Blog</Link>
+            <nav>
+                <div className='md:hidden' onClick={() => setOpen(!open)}>
+                    {
+                        open ?
+                            <FontAwesomeIcon className='h-6 w-6 md:hidden' icon={faXmark} />
+                            : <FontAwesomeIcon className='h-6 w-6 md:hidden' icon={faBars} />
+                    }
+                </div>
+
+
+                <div className={`text-xl flex flex-col md:flex-row justify-center w-full bg-rose-300 absolute md:bg-white md:static duration-500 ease-out ${open ? 'left-[250px]' : 'top-[-120px]'}`}>
+
+                    <Link className='ml-12 hover:text-orange-600' to='/'>Home</Link>
+                    <Link className='ml-12 hover:text-orange-600' to='/statistic'>Statistic</Link>
+                    <Link className='ml-12 hover:text-orange-600' to='/topics'>Topics</Link>
+                    <Link className='ml-12 hover:text-orange-600' to='/blog'>Blog</Link>
+
+                </div>
 
 
             </nav>
