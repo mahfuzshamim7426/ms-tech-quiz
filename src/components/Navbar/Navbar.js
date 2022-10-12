a33d94a81414eb7746bf59563aa4eb24def8e94c
mahfuzshamim7426 <mahfuzshamim7426@gmail.com>
2022-10-12 18:42:35 +0600

    navbar fixed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,21 +14,27 @@ const Navbar = () => {
                 </div>
             </div>
             <nav>
-                <div className='md:hidden' onClick={() => setOpen(!open)}>
+                {/* <div className='md:hidden' onClick={() => setOpen(!open)}>
                     {
                         open ?
                             <FontAwesomeIcon className='h-6 w-6 md:hidden' icon={faXmark} />
                             : <FontAwesomeIcon className='h-6 w-6 md:hidden' icon={faBars} />
                     }
+                </div> */}
+                <div className='md:hidden' onClick={() => setOpen(true)}>
+                    <FontAwesomeIcon className='h-8 w-8 md:hidden' icon={faBars} />
                 </div>
 
 
-                <div className={`text-xl flex flex-col md:flex-row md:gap-12 justify-center w-full bg-rose-300 absolute md:bg-white md:static duration-500 ease-out ${open ? 'left-[250px]' : 'top-[-120px]'}`}>
+                <div className={`p-3 text-xl flex flex-col md:flex-row md:gap-12 justify-center w-full bg-rose-200 absolute md:bg-white md:static duration-500 ease-out  left-0 ${open ? 'top-0' : '-top-full'}`}>
+                    <div className='flex justify-end md:hidden' onClick={() => setOpen(false)}>
+                        <FontAwesomeIcon className='h-8 w-8 md:hidden' icon={faXmark} />
+                    </div>
 
-                    <Link to='/'>Home</Link>
-                    <Link to='/statistic'>Statistic</Link>
-                    <Link to='/topics'>Topics</Link>
-                    <Link to='/blog'>Blog</Link>
+                    <Link className='text-center' to='/'>Home</Link>
+                    <Link className='text-center mt-2 md:mt-0' to='/statistic'>Statistic</Link>
+                    <Link className='text-center mt-2 md:mt-0' to='/topics'>Topics</Link>
+                    <Link className='text-center mt-2 md:mt-0' to='/blog'>Blog</Link>
 
                 </div>
 
